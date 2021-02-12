e003a016c7a6ce35c2c620214fde91040f76d71a
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2021-02-13 00:28:23 +0100

    en cours

--- plans.py
+++ plans.py
@@ -13,13 +13,8 @@ class Plan:
 			if isinstance(a, vecteurs.Vecteur) and isinstance(b, vecteurs.Vecteur):
 				if not(a.est_nul() or b.est_nul()):
 					if not vecteurs.collineaire(a, b):
-						print("ok")
-					else:
-						print(1)
-				else:
-					print(2)
-		else:
-			print(3)
+						
 
-
-p = Plan(vecteurs.Vecteur(0, 1, 0), vecteurs.Vecteur(1, 0, 0))
+u = vecteurs.vecteur_unitaire_x
+v = vecteurs.vecteur_unitaire_y
+p = Plan(u, v)
