1f64a1697aecb66a79e918ee70cecec756772570
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2021-02-14 01:01:48 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from droites import *
+from plans import *
+from points import *
+from tracer import *
+from utils import *
+from vecteurs import *
+
+__all__ = ("Droite", "axe_x", "axe_y", "axe_z", "parallelles", "secantes", "orthogonales",
+	"Plan", "plan_xy", "plan_yz", "plan_xz",
+	"Point", "origine", "distance", "est_meme_point", "alignes",
+	"Trace3D",
+	"MIN_DELTA", "CHIFFRES_SIGNIFICATIFS",
+	"Vecteur", "vecteur_unitaire_x", "vecteur_unitaire_y", "vecteur_unitaire_z", "vecteur_nul", "collineaires")
