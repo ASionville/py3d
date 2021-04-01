fd10ed7eec80eb388933f7a0cc89f3111d6c257a
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2021-04-01 18:17:35 +0300

    Ajout calculs

--- __init__.py
+++ __init__.py
@@ -9,10 +9,12 @@ from points import *
 from repere import *
 from utils import *
 from vecteurs import *
+from calculs import *
 
 __all__ = ("Droite", "axe_x", "axe_y", "axe_z", "parallelles", "secantes", "orthogonales",
 	"Plan", "plan_xy", "plan_yz", "plan_xz",
 	"Point", "origine", "distance", "est_meme_point", "alignes",
 	"Repere3D",
 	"MIN_DELTA", "CHIFFRES_SIGNIFICATIFS",
-	"Vecteur", "vecteur_unitaire_x", "vecteur_unitaire_y", "vecteur_unitaire_z", "vecteur_nul", "collineaires", "orthogonaux")
+	"Vecteur", "vecteur_unitaire_x", "vecteur_unitaire_y", "vecteur_unitaire_z", "vecteur_nul", "collineaires", "orthogonaux",
+	"intersection", "intersection_plan_plan", "intersection_droite_plan", "intersection_droite_droite")
